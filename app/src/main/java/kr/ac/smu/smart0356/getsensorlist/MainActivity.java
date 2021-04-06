2fcc4fd440aca8ae85de96ef095a5c4bbfef2903
ParkJH106 <jeongha4209@naver.com>
2021-04-06 09:40:13 +0900

    initial commit

---
+++ app/src/main/java/kr/ac/smu/smart0356/getsensorlist/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.smu.smart0356.getsensorlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
